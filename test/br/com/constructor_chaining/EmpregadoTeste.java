c765e549dbbc3b4ec6040a021dd5004dedfc7fbf
Kauan Mocelin <kauanmocelin@gmail.com>
2019-08-27 14:36:34 -0300

    Projeto inicial

---
+++ test/br/com/constructor_chaining/EmpregadoTeste.java
@@ -0,0 +1,21 @@
+package br.com.constructor_chaining;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 
+ * @author kmocelin
+ * @since 27/08/2019
+ */
+public class EmpregadoTeste {
+
+	@Test
+	public void deveConstruirObjetoUtilizandoEncadeamentoDeConstrutores() {
+		Empregado empregado = new Empregado();
+
+		Assert.assertEquals(empregado.getNome(), "João");
+		Assert.assertEquals(empregado.getSalario(), "1200,00");
+		Assert.assertEquals(empregado.getEndereco(), "Curitiba");
+	}
+}
